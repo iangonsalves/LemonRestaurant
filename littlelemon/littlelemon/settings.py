7f225acddebeac8067f5f8ea929e729be168c814
Ian <iangonsalves29@gmail.com>
2025-02-17 16:58:53 +0400

    updated template and static mapping

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -127,10 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'restaurant/static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    "restaurant/static/",
+   os.path.join(BASE_DIR, 'restaurant/static/'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
